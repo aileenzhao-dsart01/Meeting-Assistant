79eea75743cda2bf8052a1891df55aeed6f6b87d
Ran Zhao <aileen.zhao0129@gmail.com>
2026-06-05 21:39:42 -0400

    revice commit

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,8 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
 
-const adapter = new PrismaLibSql({
-  url: "file:./prisma/dev.db",
-});
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
